e930365f216aeb18592c17c18ebbefed6575116c
jobsteam <jobsteam@yandex.ru>
2018-05-02 19:34:34 +0000

    Настройки для деплоя проекта

--- fabfile.py
+++ fabfile.py
@@ -5,11 +5,11 @@ from key_market.apps.fabric_utils.fabric_class import (
 
 
 class Fabric(DjangoFabric):
-    host = '89.223.26.151'
-    app_name = 'key_market'
-    repository = 'git@github.com:jobsteam/steamcool.git'
-    remote_db_name = 'steamcool'
-    local_db_name = 'key_market'
+    host = '89.223.31.37'
+    app_name = 'bizon'
+    repository = 'git@github.com:jobsteam/bizon.git'
+    remote_db_name = 'bizon'
+    local_db_name = 'bizon'
     use_bower = True
     user = 'dimi'
 
